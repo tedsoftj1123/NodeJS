1ca2dcce8cef0f46e44eeed8b15de2f09665b7de
이하성 <tedsoftj1123@gmail.com>
2022-10-31 19:40:13 +0900

    📑 :: post, get

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import express from "express";
 const app = express();
-
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); //body parser for POST
+var router = express.Router();
 console.log("Heelo NODEJS");
 
+app.get("/get", (req, res) => {
+    res.json("test");
+});
+
+app.post("/post", (req, res) => {
+    res.json(req.baseUrl)
+});
+
 app.listen(3000);
+
