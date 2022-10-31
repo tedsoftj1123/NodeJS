5ce0f816a5e88724dfcee284e23bcb612d6b05d0
이하성 <tedsoftj1123@gmail.com>
2022-10-31 22:56:46 +0900

    📑 :: nestJS

---
+++ nest-js/src/main.ts
@@ -0,0 +1,14 @@
+import { Get, Param } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+
+@Get('/get')
+getString : string {
+  return "hello"
+}
+bootstrap();
