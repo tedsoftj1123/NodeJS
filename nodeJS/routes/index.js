10b3bdd84c8e9c48dca6bad0cb92c7de99d4fe8d
이하성 <tedsoftj1123@gmail.com>
2022-11-16 23:50:51 +0900

    .

--- nodeJS/routes/index.js
+++ nodeJS/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/a', function(req, res, next)) {
+  res.render()
+}
 module.exports = router;
